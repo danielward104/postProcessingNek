a8d602413b9ca6d21c65fb26cdc6199151742e27
Daniel Ward [RPG] <scdrw@comp-pc6076.leeds.ac.uk>
2018-08-24 13:31:25 +0100

    New submit technique using switches.

--- postProcess.py
+++ postProcess.py
@@ -1,36 +1,118 @@
 #Link to path in which postProcess_lib is stored.
 import sys
 import os
-sys.path.insert(1,'/home/cserv1_a/soc_pg/scdrw/Documents/nbudocuments/PhD/SimNumerics/Python/postProcessingLib')
+sys.path.insert(1,'/home/ufaserv1_g/scdrw/Python')
 import postProcess_lib as pp
 
-pp.PseudoColourPlotting( 'plume_v2_3D', 
-7,	# Order 
-3, 	# Dimension
-2, 	# Start file
-1, 	# Jump
-2, 	# Final timestep
-1, 	# Number of plots
-80, 	# Elements in x
-40, 	# Elements in y
-10, 	# Elements in z
--5, 	# x lower boundary
-5, 	# x upper boundary
-0, 	# y lower boundary
-5,	# y upper boundary
-0.25, 	# Position of z slice
-0.98, 	# Clustering in x
-0.98, 	# Clustering in y
-0 	# Particles (0 - no, 1 - yes)
-)
+# Chooses which postprocessing script to run.
+# 0 - PseudoColourPlotting
+# 1 - integrateDomain
+# 2 - integratePlume
 
-#def PseudoColourPlotting( filename, order, dimension, start_file, jump, total_timesteps, numPlots, elements_x, elements_y, elements_z, x_start, x_end, y_start, y_end, z_slice, x_cluster, y_cluster, gridType, particles ):
+switch = 0
+
+def pseudocolour():
+        pp.PseudoColourPlotting( 'plume_v3_particles',
+        7,      # Order 
+        3,      # Dimension
+        1,      # Start file
+        10,     # Jump
+        1954,   # Final timestep
+        2,      # Number of plots
+        80,     # Elements in x
+        80,     # Elements in y
+        10,     # Elements in z
+        -5,     # x lower boundary
+        5,      # x upper boundary
+        0,      # y lower boundary
+        10,     # y upper boundary
+        0.25,   # Position of z slice
+        0.98,   # Clustering in x
+        0.98,   # Clustering in y
+        0       # Particles (0 - no, 1 - yes)
+        )
+        return
+
+def integrateDomain():
+        pp.integrateDomain( 'plume_v2_3D',
+        7,      # Order
+        3,      # Dimension
+        1,     # Jump
+        1,      # Final timestep
+        80,     # Elements in x
+        40,     # Elements in y
+        10,     # Elements in z
+        -5,     # x lower boundary
+        5,      # x upper boundary
+        0,      # y lower boundary
+        5,      # y upper boundary
+        0,      # z lower boundary
+        0.5,    # z upper boundary
+        0.98,   # Clustering in x
+        0.98,   # Clustering in y
+        0       # Particles (0 - no, 1 - yes)
+        )
+        return
+
+def integratePlume():
+        pp.integratePlume( 'plume_v2_3D',
+        7,      # Order
+        3,      # Dimension
+        1,     # Jump
+        1,      # Final timestep
+        80,     # Elements in x
+        40,     # Elements in y
+        10,     # Elements in z
+        -5,     # x lower boundary
+        5,      # x upper boundary
+        0,      # y lower boundary
+        5,      # y upper boundary
+        0,      # z lower boundary
+        0.5,    # z upper boundary
+        0.98,   # Clustering in x
+        0.98,   # Clustering in y
+        0       # Particles (0 - no, 1 - yes)
+        )
+        return
+
+def plotparticlesonly():
+        pp.plotparticlesonly(
+        7,      # Order 
+        1,      # Start file
+        50,     # Jump
+        8140,   # Final particle file 
+        80,     # Elements in x
+        80,     # Elements in y
+        10,     # Elements in z
+        -5,     # x lower boundary
+        5,      # x upper boundary
+        0,      # y lower boundary
+        10,     # y upper boundary
+        0.25,   # Position of z slice
+        0.98,   # Clustering in x
+        0.98,   # Clustering in y
+        )
+        return
+
+def choose_function(argument):
+        switcher = {
+                0: pseudocolour,
+                1: integrateDomain,
+                2: integratePlume,
+                3: plotparticlesonly,
+        }
+        # Get the function from switcher dictionary
+        func = switcher.get(argument)
+
+        return func()
+
+choose_function(switch)
+
+
 
 #pp.meshPlot( 160, 160, 10, 10, 0.9, 0.92, 7 )
 
 #pp.time_finder( 'plume_v9_meshInd', 1, 8000 )
-
-#pp.integrateDomain( 'plume_v3_stratified', 200, 15000, 40, 80, 50, 100, 0.8, 1.1, 0 )
 
 # Combine images into video using the script make_video_from_image.sh
 #os.system('./make_video_from_image.sh')
